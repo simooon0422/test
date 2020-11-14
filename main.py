1271ab85c257b8b1e60d604e45ea050f9fe4f9fb
Szymon <szymonh.dg@gmail.com>
2020-11-14 15:32:29 +0100

    FirstBranch

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import xlsxwriter
 workbook = xlsxwriter.Workbook('hello.xlsx')
 worksheet = workbook.add_worksheet()
 
-worksheet.write('A1', 'Hello world!!!!!')
+worksheet.write('A10', 'A11' 'Hello world!!!!!')
 
 workbook.close()
